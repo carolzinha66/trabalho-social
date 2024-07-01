3262e695bb5edeb7b4817f8efe031bb4474148c5
carolzinha66 <108192978+carolzinha66@users.noreply.github.com>
2024-07-01 08:46:48 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,28 +6,55 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Como garantir acesso igualitário à educação de qualidade para todos?",
-        alternativas: [
-            "Investindo em infraestrutura escolar, especialmente em áreas rurais e desfavorecidas.",
-            "Criar leis que garantam o acesso a educação gratuita e de qualidade"
+        enunciado:"Como garantir acesso igualitário à educação de qualidade para todos"?
+        alternativas:[ "Investindo em infraestrutura escolar, especialmente em áreas rurais e desfavorecidas.", "Criar leis que garantam o acesso a educação gratuita e de qualidade",
+            {
+                texto: "Isso é assustador!",
+                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+            },
+            {
+                texto: "Isso é maravilhoso!",
+                afirmacao: "Quis saber como usar IA"
+            },
         ]
     },
 ];
-
+                
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
+
 function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
     mostraAlternativas();
 }
-function mostraAlternativas() {
-    for(const alternativa of perguntaAtual.alternativas){
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativa.textContent = alternativa;
-        caixaAlternativas.appendChild (botaoAlternativas);
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
 
-mostraPergunta();
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
 
+function mostraResultado() {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+
+mostraPergunta();
